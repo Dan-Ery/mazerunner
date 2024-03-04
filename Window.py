0628b974816da3cd0bde50a4357a27eb5b0bb2d9
Dan-Ery <daniel.eryavec@gmail.com>
2024-03-04 02:53:30 -0500

    feat: Add Point and Line

--- Window.py
+++ Window.py
@@ -19,3 +19,19 @@ class Window:
 
     def close(self):
         self.running=False
+
+    def draw_line(self, line, fill_color):
+        line.draw(self.canvas, fill_color)
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    
+class Line:
+    def __init__(self, point_a, point_b):
+        self.point_a = point_a
+        self.point_b = point_b
+
+    def draw(self, canvas, fill_color):
+        canvas.create_line(self.point_a.x, self.point_a.y, self.point_b.x, self.point_b.y, fill_color, 2)
