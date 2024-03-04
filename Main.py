c6af2edbcfdc9f327bf4c78171e74e9572b7e0fe
Dan-Ery <daniel.eryavec@gmail.com>
2024-03-04 03:26:40 -0500

    test: Test Cell.draw

--- Main.py
+++ Main.py
@@ -1,11 +1,11 @@
 from Window import *
-
+from Maze import *
 def main():
     win = Window(800, 600)
 
     point_a, point_b = Point(0,0), Point(10,10)
-    line = Line(point_a, point_b)
-    win.draw_line(line, "red")
+    cell = Cell(win)
+    cell.draw(5,5,40,40)
 
     win.wait_for_close()
 
