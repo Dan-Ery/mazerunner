5b4f318bbfc583d32fff89197e8c77bcf3c02748
Dan-Ery <daniel.eryavec@gmail.com>
2024-03-04 04:11:59 -0500

    test: Test maze

--- Main.py
+++ Main.py
@@ -3,11 +3,7 @@ from Maze import *
 def main():
     win = Window(800, 600)
 
-    cell1 = Cell(win)
-    cell1.draw(5,5,40,40)
-    cell2 = Cell(win)
-    cell2.draw(55,55,80,80)
-    cell1.draw_move(cell2)
+    maze = Maze(5,5,3,5,30,20,win)
 
     win.wait_for_close()
 
--- Maze.py
+++ Maze.py
@@ -1,4 +1,56 @@
 from Window import *
+import time
+
+class Maze:
+    def __init__(
+        self,
+        x1,
+        y1,
+        num_rows,
+        num_cols,
+        cell_size_x,
+        cell_size_y,
+        win,
+    ):
+        self.x1=x1
+        self.y1=y1
+        self.num_rows=num_rows
+        self.num_cols=num_cols
+        self.cell_size_x=cell_size_x
+        self.cell_size_y=cell_size_y
+        self.window=win
+        
+        self._create_cells()
+    
+    def _create_cells(self):
+        cell_matrix = []
+        for col in range(0, self.num_cols):
+            column = []
+            for row in range(0, self.num_rows):
+                cell = Cell(self.window)
+                column.append(cell)
+            cell_matrix.append(column)
+
+        self.cells = cell_matrix
+        
+        for col in range(0, self.num_cols):
+            for row in range(0, self.num_rows):
+                self._draw_cell(col, row)
+
+    def _draw_cell(self, col, row):
+        x1 = self.x1 + self.cell_size_x * col
+        x2 = x1 + self.cell_size_x
+        y1 = self.y1 + self.cell_size_y * row
+        y2 = y1+ self.cell_size_y
+        
+        cell = self.cells[col][row]
+        cell.draw(x1,y1,x2,y2)
+        self._animate()
+
+    def _animate(self):
+        self.window.redraw()
+        time.sleep(0.05)
+        
 
 class Cell:
     def __init__(self, win):
